fa1680d7322a26f5d2367fad2936f0a8ae542d9a
Lebot Apps <lebot.llc@gmail.com>
2021-06-09 08:07:34 -0400

    added seed to index and model

--- server.js
+++ server.js
@@ -35,40 +35,19 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 // 		res.redirect('/products');
 // 	});
 // });
+//Seed
 app.get('/products/seed', (req, res) => {
-    Product.create(
-[{
-    
-    name: 'Beans',
-    description: 'A small pile of beans. Buy more beans for a big pile of beans.',
-    img: 'https://imgur.com/LEHS8h3.png',
-    price: 5,
-    qty: 99
-  }, {
-    name: 'Bones',
-    description: 'It\'s just a bag of bones.',
-    img: 'https://imgur.com/dalOqwk.png',
-    price: 25,
-    qty: 0
-  }, {
-    name: 'Bins',
-    description: 'A stack of colorful bins for your beans and bones.',
-    img: 'https://imgur.com/ptWDPO1.png',
-    price: 7000,
-    qty: 1
-},
-],
-(error, data) => {
-    res.redirect('/products');
-}
-);
-});
+	Product.deleteMany({}, (error, allProduct) => {});
 
+	Product.create(productSeed, (error, data) => {
+		res.redirect('/products');
+	});
+});
 //INDUCES 
 
 //Index
 
-app.get('/products', (req, res) => {
+app.get('/products/', (req, res) => {
     Product.find({}, (error, allProduct) =>{
         res.render('index.ejs', {
             Product: allProduct,
