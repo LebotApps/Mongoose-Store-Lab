d661a826aff0f8c973a9efb915d3ea67b11b87f8
Lebot Apps <lebot.llc@gmail.com>
2021-06-09 09:03:11 -0400

    updated seed and index

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 // Dependencies 
 
 const express = require('express');
@@ -35,14 +36,34 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 // 		res.redirect('/products');
 // 	});
 // });
-//Seed
 app.get('/products/seed', (req, res) => {
-	Product.deleteMany({}, (error, allProduct) => {});
-
-	Product.create(productSeed, (error, data) => {
-		res.redirect('/products');
-	});
+    Product.create(
+    [{
+    name: 'Beans',
+    description: 'A small pile of beans. Buy more beans for a big pile of beans.',
+    img: 'https://imgur.com/LEHS8h3.png',
+    price: 5,
+    qty: 99
+  }, {
+    name: 'Bones',
+    description: 'It\'s just a bag of bones.',
+    img: 'https://imgur.com/dalOqwk.png',
+    price: 25,
+    qty: 0
+  }, {
+    name: 'Bins',
+    description: 'A stack of colorful bins for your beans and bones.',
+    img: 'https://imgur.com/ptWDPO1.png',
+    price: 7000,
+    qty: 1
+},
+],
+(error, data) => {
+    res.redirect('/books');
+}
+);
 });
+
 //INDUCES 
 
 //Index
