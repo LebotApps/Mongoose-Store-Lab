7cb204eea4084fea681c2ba6894767f51eb8c4c5
Lebot Apps <lebot.llc@gmail.com>
2021-06-09 07:57:41 -0400

    added seed

--- server.js
+++ server.js
@@ -1,49 +1,72 @@
-//Dependencies
+// Dependencies 
 
-const express = require ('express');
-const mongoose = require ('mongoose');
-
-//Require Model 
-
-const Product = require ('./models/products.js');
-
-//Initialize the Express App
-
+const express = require('express');
 const app = express ();
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Product = require('./models/products.js');
+const Seed = require('./models/productSeed.js');
 
-//Port Value
+//Middleware
+//Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended:true}));
 
-const PORT = 3000;
+//Database Connection
 
-//Mount our middleware functions
-
-app.use(express.urlencoded({extended:false}));
-
-//Configure Mongoose 
-
-const DATABASE_URL = 'mongodb+srv://admin:abc1234@products.jnypw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
-const db = mongoose.connection;
-
-//GET INFOR FOR MONGOOSE CONNECT
-
-mongoose.connect(DATABASE_URL, {
+mongoose.connect(process.env.DATABASE_URL, { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
 });
 
-//listen for mongodb events
-db.on('error', (error)=>
-    console.log(error.message + ' is mongodb not running?'));
-db.on('connected', ()=> console.log('mongodb connected'));
-db.on('disconnected', ()=> console.log('mongodb disconnected'));
+// Database Connection Error/Success
+// Define callback functions for various events
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
-//Set routes and controller code 
-//INDUCES
+// Seed
+// app.get('/products/seed', (req, res) => {
+// 	Product.deleteMany({}, (error, allProduct) => {});
 
-// Index (Read Route)
+// 	Product.create(productSeed, (error, data) => {
+// 		res.redirect('/products');
+// 	});
+// });
+app.get('/products/seed', (req, res) => {
+    Product.create(
+[{
+    
+    name: 'Beans',
+    description: 'A small pile of beans. Buy more beans for a big pile of beans.',
+    img: 'https://imgur.com/LEHS8h3.png',
+    price: 5,
+    qty: 99
+  }, {
+    name: 'Bones',
+    description: 'It\'s just a bag of bones.',
+    img: 'https://imgur.com/dalOqwk.png',
+    price: 25,
+    qty: 0
+  }, {
+    name: 'Bins',
+    description: 'A stack of colorful bins for your beans and bones.',
+    img: 'https://imgur.com/ptWDPO1.png',
+    price: 7000,
+    qty: 1
+},
+],
+(error, data) => {
+    res.redirect('/products');
+}
+);
+});
+
+//INDUCES 
+
+//Index
 
 app.get('/products', (req, res) => {
     Product.find({}, (error, allProduct) =>{
@@ -54,23 +77,39 @@ app.get('/products', (req, res) => {
     });
 });
 
-//NEW
 
-app.get('/products/new', (reg, res)=>{
-   
-        res.render('new.ejs');
+//New
+app.get('/products/new', (req, res)=>{
+    res.render('new.ejs');
+})
+//Delete
+
+//Update
+
+//Create
+app.post('/products', (req, res) => {
+    if(req.body.completed === 'on') {
+        //if checked, req.body.completed is set to 'on'
+        req.body.completed = true;
+    } else {
+        //if not checked, req.body.completed is undefined
+        req.body.completed = false;
+    }
+    Product.create(req.body, (error, createdProduct)=>{
+        res.redirect('/products');
     });
+});
+//Edit
+
+//Show
+
+app.get('/products/:id', (req, res) =>{
+    Product.findById(req.params.id, (err, foundProduct) =>{
+        res.render('show.ejs', {log: foundProduct});
+    });
+});
 
 
-//DELETE
-
-//UPDATE
-
-//CREATE
-
-//EDIT
-
-//SHOW
-
-//Tell Express To Listen
-app.listen(PORT, () => console.log(`express is listening on port ${PORT}`)); 
+//Listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
